2605213a6162407915938ed4578bfc442f5c9d2b
Mohammed mujahith <mohammedmujahith.ai25@stellamaryscoe.edu.in>
2026-02-21 14:29:41 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,47 +1,40 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAX6iBehOjRGDbyE3HWUWXUsdIREZcICwM",
-  authDomain: "smartai-private.firebaseapp.com",
-  projectId: "smartai-private",
-  storageBucket: "smartai-private.firebasestorage.app",
-  messagingSenderId: "52903428329",
-  appId: "1:52903428329:web:478d1a15e4dbfaa38c3078",
-  measurementId: "G-FBLR8XVZTB"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-window.recaptchaVerifier = new RecaptchaVerifier(auth, 'recaptcha-container', {
-  size: 'normal'
-});
+// SIGN UP
+window.signup = function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-window.sendOTP = function () {
-  const phoneNumber = document.getElementById('phoneNumber').value;
-  const appVerifier = window.recaptchaVerifier;
-
-  signInWithPhoneNumber(auth, phoneNumber, appVerifier)
-    .then((confirmationResult) => {
-      window.confirmationResult = confirmationResult;
-      alert("OTP Sent!");
-    })
-    .catch((error) => {
-      alert(error.message);
-    });
-};
-
-window.verifyOTP = function () {
-  const code = document.getElementById('otp').value;
-
-  window.confirmationResult.confirm(code)
-    .then((result) => {
-      localStorage.setItem("user", result.user.phoneNumber);
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      alert("Account Created!");
       window.location.href = "chat.html";
     })
-    .catch((error) => {
-      alert("Invalid OTP");
-    });
+    .catch((error) => alert(error.message));
+};
+
+// LOGIN
+window.login = function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      window.location.href = "chat.html";
+    })
+    .catch((error) => alert(error.message));
 };
